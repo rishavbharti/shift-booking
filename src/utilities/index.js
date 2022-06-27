c556c30f37ad3bd6da24948d07384ff42d761b78
Rishav Bharti <rishavbharti21@gmail.com>
2022-06-27 20:22:48 +0530

    Create utility functions for formatting date

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -3,3 +3,19 @@ export const getTimeInHoursMinutes = (time) =>
     hour: '2-digit',
     minute: '2-digit',
   });
+
+export const getDate = (date) => new Date(date).toLocaleDateString();
+
+export const getMonthAndDate = (date) =>
+  new Date(date).toLocaleDateString(navigator.language, {
+    month: 'long',
+    day: 'numeric',
+  });
+
+export const getTodayDate = () => getDate(new Date());
+
+export const getTomorrowDate = () => {
+  let today = new Date();
+  let tomorrow = today.setDate(today.getDate() + 1);
+  return getDate(tomorrow);
+};
